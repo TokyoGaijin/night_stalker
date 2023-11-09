42466bfb65ec200a332c70a0aabee51121babfe4
TokyoGaijin <120317383+TokyoGaijin@users.noreply.github.com>
2023-11-09 23:36:21 +0900

    GUI work and implementing victims

--- night-stalker.py
+++ night-stalker.py
@@ -15,12 +15,23 @@ BG = (0, 0, 0)
 ALL_LINES = (255, 255, 255)
 
 # Classes and Object Calls will go here
+class Writer:
+    def __init__(self):
+        self.SIZE = 40
+        self.FONT = pygame.font.Font("ARCADECLASSIC.TTF", self.SIZE)
+        
+    def write(self, string_to_write, coords):
+        showtext = self.FONT.render(string_to_write, self.SIZE, ALL_LINES)
+        SURFACE.blit(showtext, coords)
+
 class GUI:
     # Management for score display and the life meter
     def __init__(self):
+        self.pen = Writer()
         self.life_rect = pygame.Rect(20, 60, 200, 30)
 
     def draw(self):
+        pen.write("LIFE", (20, 18))
         pygame.draw.rect(SURFACE, ALL_LINES, self.life_rect)
 
 
@@ -38,8 +49,8 @@ class BackState(Enum):
     MOUNTAINS = 1
 
 class Background:
-    def __init__(self):
-        startX, startY = 800, 300 - 40
+    def __init__(self, startX = 800, startY = 300 - 40, state = BackState.MOUNTAINS):
+        self.startX, self.startY = startX, startY
         self.back_images = [["--------------------------------------------------",
                              "--------------------------------------------------",
                              "--------------------------------------00----------",
@@ -62,6 +73,16 @@ class Background:
                              "0-0-0-0-0-0--0-0-0--0--00--0--0--00--0--0--0-0-0-0"],
 
                             ["--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
+                             "--------------------------------------------------",
                              "-----------------------o--------------------------",
                              "--------------0------00-00------0-----------------",
                              "------00-----0-0----0-----0---00-0----------------",
@@ -72,7 +93,7 @@ class Background:
                              "-0----------0--0---------------0--0---------00--0-",
                              "0------------00-----------------0------------0---0",]]
 
-        self.current_state = BackState.MOUNTAINS
+        self.current_state = state
 
         self.city_image = []
         self.mount_image = []
@@ -102,17 +123,19 @@ class Background:
     def draw(self):
         if self.current_state == BackState.MOUNTAINS:
             for i in self.mount_image:
-                i.rect.draw()
+                i.draw()
         if self.current_state == BackState.CITY:
             for i in self.city_image:
-                i.rect.draw()
+                i.draw()
 
     def update(self):
         if self.current_state == BackState.MOUNTAINS:
-            pass
+            for pixel in self.mount_image:
+                pixel.rect.x -= 3
         if self.current_state == BackState.CITY:
-            pass
-
+            for pixel in self.city_image:
+                pixel.rect.x -= 3
+                
         
 
 
@@ -137,10 +160,10 @@ class HeroState(Enum):
 class NightStalker:
     # The Hero Class
     def __init__(self, startX, startY):
-        self.SPEED = 5
+        self.speed = 5
         self.current_state = HeroState.LANDED
         self.score = 0
-
+        pen = Writer()
         self.hit_box = pygame.Rect(startX, startY, 50, 26)
         self.blueprint = [["00000--------------------",
                           "-0---0--------0000-------",
@@ -175,6 +198,7 @@ class NightStalker:
         self.bullet_list = []
         self.fire_timer = 0
         self.COOLDOWN = 60
+        
 
         self.display = GUI()
 
@@ -199,10 +223,6 @@ class NightStalker:
                 x = startX
 
         build_cars()
-
-
-    
-        
 
     def move(self, axis, speed):
         for pixel in self.driving_mode:
@@ -251,7 +271,7 @@ class NightStalker:
         KEYS = pygame.key.get_pressed()
        
         if self.current_state != HeroState.DEAD:
-            if self.driving_mode[0].rect.y < 300:
+            if self.driving_mode[0].rect.y < 350:
                 self.current_state = HeroState.FLYING
                 self.dead_car = self.flying_mode
             else:
@@ -264,13 +284,13 @@ class NightStalker:
 
         if self.current_state != HeroState.DEAD:
             if KEYS[pygame.K_a]:
-                self.move("x", -self.SPEED)
+                self.move("x", -self.speed)
             if KEYS[pygame.K_d]:
-                self.move("x", self.SPEED)
+                self.move("x", self.speed)
             if KEYS[pygame.K_w]:
-                self.move("y", -self.SPEED)
+                self.move("y", -self.speed)
             if KEYS[pygame.K_s]:
-                self.move("y", self.SPEED)
+                self.move("y", self.speed)
             if KEYS[pygame.K_SPACE]:
                 if self.fire_timer == 0 and len(self.bullet_list) < 3:
                     self.bullet_list.append(Bullet(self.dead_car[37].rect.x - 5, self.dead_car[37].rect.y))
@@ -287,15 +307,51 @@ class NightStalker:
             bullet.update()
             if bullet.rect.x > 800:
                 self.bullet_list.remove(bullet)
+                
+        if self.dead_car[-1].rect.bottom >= 450 and KEYS[pygame.K_s]:
+            self.speed = 0
+        else:
+            self.speed = 5
+            
+        if self.dead_car[-1].rect.bottom > 454:
+            self.current_state = HeroState.DEAD
+            
+        pen.write(f"SCORE {self.score}", (550,18))
 
-
-
-
-
-
-
-
-
+class Victim:
+    def __init__(self, startX, startY):
+        self.hit_box = pygame.Rect(startX, startY, 20, 20)
+        self.image = ["0---00---0",
+                      "-0-0000-0-",
+                      "--0-00-0--",
+                      "---0000---",
+                      "----00----",
+                      "----00----",
+                      "---0--0---",
+                      "--0----0--",
+                      "--0----0--",
+                      "000----000"]
+        self.victim = []
+        def make_victim():
+            x, y = startX, startY
+            for row in self.image:
+                for bodypart in row:
+                    if bodypart == "0":
+                        self.victim.append(Pixel(x, y))
+                    x += 2
+                y += 2
+                x = startX
+        make_victim()
+        
+    def draw(self):
+        for part in self.victim:
+            part.draw()
+            
+    def update(self):
+        for part in self.victim:
+            part.rect.x -= 4
+        self.hit_box.x -= 4
+        
 
 
 
@@ -307,7 +363,9 @@ pygame.display.set_caption(NAME)
 
 # Game subroutines go here
 player = NightStalker(400, 300)
-
+pen = Writer()
+bg_list = []
+victim_list = []
 
 
 
@@ -318,8 +376,15 @@ def die():
         sys.exit(1)
 
 def draw():
+    for bgs in bg_list:
+        bgs.draw()
     # Horizon Line
     pygame.draw.rect(SURFACE, ALL_LINES, pygame.Rect(0, 300, 800, 2))
+    # Road lanes
+    pygame.draw.rect(SURFACE, ALL_LINES, pygame.Rect(0, 350, 800, 2))
+    pygame.draw.rect(SURFACE, ALL_LINES, pygame.Rect(0, 450, 800, 2))
+    for victim in victim_list:
+        victim.draw()
     player.draw()
 
 def update():
@@ -328,10 +393,30 @@ def update():
     for event in pygame.event.get():
         if event.type == pygame.QUIT or KEYS[pygame.K_LCTRL] and KEYS[pygame.K_q]:
             die()
-
+    
     draw()
 
+    if len(bg_list) < 2:
+        bg_list.append(Background(startX = random.randint(900, 1200), state=random.choice([BackState.CITY, BackState.MOUNTAINS])))
+        bg_list.append(Background(startX = random.randint(1400, 2000), state=random.choice([BackState.CITY, BackState.MOUNTAINS])))
+
+    if len(victim_list) < 4:
+        victim_list.append(Victim(random.randint(900, 2000), random.randint(350, 450)))
+        
+    for victim in victim_list:
+        victim.update()
+        if victim.hit_box.colliderect(player.hit_box):
+            player.score += 250
+            victim_list.remove(victim)
+        
+        if victim.hit_box.right < 0:
+            victim_list.remove(victim)
+
     # Objects' update methods go here
+    for bgs in bg_list:
+        bgs.update()
+        if bgs.mount_image[-1].rect.x <= -50 or bgs.city_image[-1].rect.x <= -50:
+            bg_list.remove(bgs)
 
 
     player.update()
