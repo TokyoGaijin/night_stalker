c85f5dec2e63a1311277989b46f8f055d78a7658
TokyoGaijin <mbfleish1105@live.com>
2023-11-09 14:11:15 +0900

    The stage is set. MD Notes began

---
+++ NightStalker[Mikes_Arcade].md
@@ -0,0 +1,14 @@
+# About: *Night Stalker*
+---
+*Night Stalker* is (c)2023-2024 Michael Yamazaki-Fleisher and is a retro-style SHMUP in the theme of late 1970s/1980s graphics.
+The hero is a car that can fly and drive on the ground to kill enemies.
+
+A story will be developed.
+
+Game will have no color except for black and white.
+
+---
+
+## Game Controls
+Game will be controlled by `W` `A` `S` `D` keys for movement, `SPACE` to fire and `E` to transform while pressing `W`
+
---
+++ night-stalker.py
@@ -0,0 +1,52 @@
+import pygame
+from pygame import mixer
+from enum import Enum
+import time
+import sys
+import pyautogui
+
+pygame.init() # for fonts and sounds
+
+SCREEN = (800, 600)
+SURFACE = pygame.display.set_mode(SCREEN)
+BG = (0, 0, 0)
+ALL_LINES = (255, 255, 255)
+
+# Classes and Object Calls will go here
+
+
+CLOCK = pygame.time.Clock()
+FPS = 60
+NAME = "Night Stalker™ by Michael Yamazaki-Fleisher ©2023-2024"
+
+
+# Game subroutines go here
+
+def die():
+    confirm = pyautogui.confirm("Are you sure you want to exit to OS?", "Confirm", ["Yes", "No"])
+    if confirm == "Yes":
+        pygame.quit()
+        sys.exit(1)
+
+def draw():
+    pass
+
+def update():
+    CLOCK.tick(FPS)
+    KEYS = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or KEYS[pygame.K_LCTRL] and KEYS[pygame.K_q]:
+            die()
+
+    draw()
+
+    # Objects' update methods go here
+    pygame.display.update()
+    SURFACE.fill(BG)
+
+def run():
+    while True:
+        update()
+
+if __name__ == '__main__':
+    run()
